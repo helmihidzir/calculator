2df0e02f3aa75b18df01cdab821a4b97cd59c1f5
helmihidzir <helmihidzir@gmail.com>
2019-07-05 18:38:32 +0800

    Learn more about RSpec

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,16 @@ class Calculator
   def add(x, y)
     x + y
   end
+
+  def subs(x, y)
+    x - y
+  end
+
+  def mult(x, y)
+    x * y
+  end
+
+  def div(x, y)
+    x / y.to_f
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,5 +5,20 @@ describe "calculator" do
     calc = Calculator.new
     expect(calc.add(2, 3)).to eql(5)
   end
+
+  it "substracts number" do
+    calc = Calculator.new
+    expect(calc.subs(5, 1)).to eql(4)
+  end
+
+  it "multiplies number" do
+    calc = Calculator.new
+    expect(calc.mult(4, 3)).to eql(12)
+  end
+
+  it "divides number" do
+    calc = Calculator.new
+    expect(calc.div(5, 2)).to eql(2.5)
+  end
 end
 
